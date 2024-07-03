8d9cacb9b6d0cfd5af06a53686e7fa10e9893c8e
bravexample <laiyongdian@icloud.com>
2024-07-03 21:40:13 +0800

    update game page

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // the object for the basic information of the game
-var basic = {
+let basic = {
     state: 0,
     target_score: 0,
     player_amount: 0,
 }
 
 // the object(s) and numbers for the player
-var players = [];
-var current_player = 0;
-var playing = 0;
+let players = [];
+let current_player = 0;
+let playing = 0;
 
 // for elements
-var body;
-var target_score;
-var player_amount;
-var player_names = [];
+let body;
+let target_score;
+let player_amount;
+let name_row;
+let score_row;
+let score;
 
 // the main function
 function main() {
@@ -48,12 +50,13 @@ function next() {
             basic.state = 1;
             break;
         case 1:
-            for (var i = 0; i < basic.player_amount; i++) {
+            for (let i = 0; i < basic.player_amount; i++) {
                 players.push({
-                    name: player_names[i].value,
+                    name: document.getElementById('player_name_' + i).value,
                     score: basic.target_score,
                 });
             }
+            playing = basic.player_amount;
             basic.state = 2;
             break;
         case 2:
@@ -70,7 +73,7 @@ function next() {
 
 }
 
-// the function for the first step
+// the function for the first page, getting the target score and the amount of players
 function welcome() {
     body.innerHTML = '<h1>歡迎使用飛鏢計分器</h1>'
                    + '<input type="number" id="target_score" placeholder="請輸入目標分數" min=301 max=701 step=200 />'
@@ -93,33 +96,56 @@ function welcome() {
     })
 }
 
+// the function for the second page, getting the names of the players
 function input_player() {
     body.innerHTML = '<h1>請輸入玩家名稱</h1>'
-    for (var i = 0; i < basic.player_amount; i++) {
-        body.innerHTML += '<input type="text" id="player_name_' + i + '" placeholder="玩家 ' + (i + 1) + ' 名稱" />'
-        player_names.push(document.getElementById('player_name_' + i));
+    for (let i = 0; i < basic.player_amount; i++) {
+        body.innerHTML += '<input id="player_name_' + i + '" placeholder="玩家 ' + (i + 1) + ' 名稱" />';
     }
 
-    player_names[0].focus();
+    let first_input = document.getElementById('player_name_0');
+    first_input.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            document.getElementById('player_name_1').focus();
+        }
+    })
+    first_input.focus();
 
-    // for (var i = 0; i < player_names.length - 1; i++) {
-    //     player_names[i].addEventListener('keypress', function(event) {
-    //         if (event.key === 'Enter') {
-    //             player_names[i + 1].focus();
-    //         }
-    //     })
-    // }
+    for (let i = 1; i < basic.player_amount - 1; i++) {
+        document.getElementById('player_name_' + i).addEventListener('keypress', function(event) {
+            if (event.key === 'Enter') {
+                document.getElementById('player_name_' + (i + 1)).focus();
+            }
+        })
+    }
 
-    // player_names[player_names.length - 1].addEventListener('keypress', function(event) {
-    //     if (event.key === 'Enter') {
-    //         next();
-    //     }
-    // })
+    document.getElementById('player_name_' + (basic.player_amount - 1)).addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            next();
+        }
+    })
 }
 
+// the function for the game
 function game() {
-    body.innerHTML = '<h1 id="h1"></h1>'
-                   + '<table id="table"></table>'
-                   + '<input type="number" id="score" min=0 max=180 />'
-                   + '<button onclick="next()">下一位</button>';
+    body.innerHTML = '<h1 id="h1">現在輪到 ' + players[current_player].name + ' 玩家</h1>'
+                   + '<table><tr id="name_row"></tr><tr id="score_row"></tr></table>'
+                   + '<input type="number" id="score" placeholder="請輸入本局分數" min=0 max=180 />';
+    
+    name_row = document.getElementById('name_row');
+    score_row = document.getElementById('score_row');
+
+    // list the players and their scores in the table
+    for (let i = 0; i < basic.player_amount; i++) {
+        name_row.innerHTML += '<td>' + players[i].name + '</td>';
+        score_row.innerHTML += '<td>' + players[i].score + '</td>';
+    }
+
+    score = document.getElementById('score');
+    score.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            next();
+        }
+    })
+    score.focus();
 }
