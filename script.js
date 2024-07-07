eb738b3a742cf4e32447d63158a3c484b7636686
bravexample <laiyongdian@icloud.com>
2024-07-07 20:39:01 +0800

    add again

--- script.js
+++ script.js
@@ -60,7 +60,6 @@ function input_player() {
     document.getElementById('player_name_' + (player_amount - 1)).addEventListener('keypress', function(event) {
         if (event.key === 'Enter') {
             add_players();
-            game();
         }
     })
 }
@@ -76,6 +75,8 @@ function add_players() {
     }
 
     playing = player_amount;
+
+    game()
 }
 
 // the function for the game
@@ -131,7 +132,8 @@ function check_score(score) {
 // the function for the result
 function result() {
     body.innerHTML = '<h1>遊戲結束</h1>'
-                   + '<table id="table"></table>';
+                   + '<table id="table"></table>'
+                   + '<button onclick="again()">再來一次</button>';
 
     var table = document.getElementById('table');
 
@@ -139,4 +141,18 @@ function result() {
         table.innerHTML += '<tr><td>第 ' + (i + 1) + ' 名</td></tr>'
                          + '<tr><td>' + rank[i] + '</td></tr>';
     }
+}
+
+// the function for playing again with the same conditions
+function again() {
+    for (let i = 0; i < player_amount; i++) {
+        players[i].score = target_score;
+        players[i].playing = true;
+    }
+
+    current_player = 0;
+    playing = player_amount;
+    rank = [];
+
+    game();
 }
